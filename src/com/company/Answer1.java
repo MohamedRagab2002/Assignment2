ced5373c964f7bbff61b10550bbf75bc23df2ced
Mohamed Ragab <82184006+MohamedRagab2002@users.noreply.github.com>
2021-10-29 23:54:58 +0200

    Answer Q1 Write a program that take number from user then print yes if that number can divide by 3 and 4 otherwise print no

--- src/com/company/Answer1.java
+++ src/com/company/Answer1.java
@@ -1,7 +1,18 @@
 package com.company;
-
+import java.util.Scanner;
 public class Answer1 {
     public static void main(String[] args) {
-        System.out.println("Answer 1");
+        int number;
+        System.out.println("Enter The Number");
+        Scanner input=new Scanner(System.in);
+        number=input.nextInt();
+        if (number%3==0&&(number&4)==0)
+        {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("NO");
+        }
+
     }
 }
