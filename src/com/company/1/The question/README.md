30ed493ba07bd73a53b6cddaf3559f6a729051ff
Mohamed Ragab <82184006+MohamedRagab2002@users.noreply.github.com>
2021-10-30 00:49:01 +0200

    تنظيم الملفات

---
+++ src/com/company/1/The question/README.md
@@ -0,0 +1,1 @@
+## Write a program that take number from user then print yes if that number can divide by 3 and 4 otherwise print no
---
+++ src/com/company/Answer3.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Answer3 {
+    // Answer
+    /*
+     *
+     */
+    public static void main(String[] args) {
+        // Question
+
+    }
+}
